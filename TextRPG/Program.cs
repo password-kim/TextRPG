533576a1aa3e81b277f959a30692dedaf71a6d65
password-kim <galaxykim0611@gmail.com>
2022-04-22 15:00:03 +0900

    TextRPG 몬스터 생성

--- TextRPG/Program.cs
+++ TextRPG/Program.cs
@@ -12,6 +12,13 @@ namespace TextRPG
             Mage = 3
         }
 
+        struct Player
+        {
+            public int hp;
+            public int attack;
+        }
+
+
         static ClassType ChooseClass()
         {
             Console.WriteLine("당신의 직업을 선택해주세요.");
@@ -42,13 +49,47 @@ namespace TextRPG
 
             return choice;
         }
+
+        static void CreatePlayer(ClassType choice , out Player player)
+        {
+            switch (choice)
+            {
+                case ClassType.Knight:
+                    player.hp = 100;
+                    player.attack = 10;
+                    break;
+                case ClassType.Archer:
+                    player.hp = 75;
+                    player.attack = 12;
+                    break;
+                case ClassType.Mage:
+                    player.hp = 50;
+                    player.attack = 15;
+                    break;
+                default:
+                    player.hp = 0;
+                    player.attack = 0;
+                    break;
+            }
+
+        }
+
         static void Main(string[] args)
         {
-            ClassType choice = ClassType.None;
-
-            while (choice == ClassType.None)
+            while (true)
             {
-                choice = ChooseClass();
+                ClassType choice = ChooseClass();
+                if(choice != ClassType.None)
+                {
+                    // 캐릭터 생성
+                    Player player;
+
+                    CreatePlayer(choice, out player);
+
+                    Console.WriteLine($"HP : {player.hp}  ATT : {player.attack}");
+
+                    // 필드입장
+                }
             }
         }
     }
